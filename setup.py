01d2ff4fa21982ea1699706b28f5df167336c729
Xiaoquan Kong <u1mail2me@gmail.com>
2019-05-16 15:00:58 +0800

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ioflow',
-    version='0.4.1',
+    version='0.4.2',
     packages=setuptools.find_packages(),
     setup_requires=[
         'tensorflow',
