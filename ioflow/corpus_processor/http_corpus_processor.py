97d3e91819d882f997ed4197df1a40d4d66027e6
Xiaoquan Kong <u1mail2me@gmail.com>
2019-05-22 10:15:12 +0800

    Bugfix

--- ioflow/corpus_processor/http_corpus_processor.py
+++ ioflow/corpus_processor/http_corpus_processor.py
@@ -13,14 +13,16 @@ from ioflow.corpus import Corpus
 def parse(obj):
     # find entity
     entity = None
-    for entity in obj['annotations']:
-        if entity['labels_type'] == 'slot':
+    for i in obj['annotations']:
+        if i['labels_type'] == 'slot':
+            entity = i
             break
 
     # find label class_
     class_ = None
-    for class_ in obj['annotations']:
-        if class_['labels_type'] == 'classify':
+    for i in obj['annotations']:
+        if i['labels_type'] == 'classify':
+            class_ = i
             break
 
     span_set = SpanSet()
@@ -28,7 +30,7 @@ def parse(obj):
         span_obj = Span(start=int(span['start_index']), end=(int(span['start_index']) + int(span['slot_len'])), entity=span['slot_value'])
         span_set.append(span_obj)
 
-    label = class_['classify']
+    label = class_['classify'] if class_ else None
     seq = Sequence(text=obj['text'], span_set=span_set, label=label)
 
     return seq
@@ -36,7 +38,8 @@ def parse(obj):
 
 def generator_fn(config):
     r = requests.post(config['data_url'], json={'taskId': config['task_id']})
-    sentence_list = r.json()['data']
+    parsed_data = r.json()
+    sentence_list = parsed_data['data']
 
     for sentence in sentence_list:
         offset_data = parse(sentence)
@@ -45,8 +48,18 @@ def generator_fn(config):
 
 
 def request_meta_data(config):
-    r = requests.get(config['meta_data_url'], params={'task_id': config['task_id']})
-    return r.json()
+    r = requests.post(config['meta_data_url'], json={'taskId': config['task_id']})
+    parsed_data = r.json()
+
+    entity_data = None
+    label_data = None
+    for i in parsed_data['data']:
+        if i['type'] == 'classify' and not label_data:
+            label_data = i['labels']
+        if i['type'] == 'slot' and not entity_data:
+            entity_data = i['slots']
+
+    return {'tags': entity_data, 'labels': label_data}
 
 
 class HttpCorpusProcessor(CorpusProcessorBase):
@@ -67,7 +80,8 @@ class HttpCorpusProcessor(CorpusProcessorBase):
 if __name__ == "__main__":
     config = {
         'data_url': 'http://10.43.10.48:8110/algo/corpusManger/getCorpusByTrainingTaskId',
-        'task_id': '5cd536215148635cc0fe29e2'
+        'meta_data_url': 'http://10.43.10.48:8110/algo/corpusManger/getLabelsInfoByTaskId',
+        'task_id': '5ce3dfe15148635a5c04a688'
     }
 
     processor = HttpCorpusProcessor(config)
@@ -76,3 +90,6 @@ if __name__ == "__main__":
 
     for i in gfunc():
         print(i)
+
+    meta_data = processor.get_meta_info()
+    print(meta_data)
