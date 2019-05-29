b76fcbfb5ff03e605a15f2df48b0c3064d214eca
Xiaoquan Kong <u1mail2me@gmail.com>
2019-05-29 15:01:56 +0800

    Add implementation

--- ioflow/task_status.py
+++ ioflow/task_status.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class TaskStatus(object):
     def __init__(self, config):
         self.DONE = 10
@@ -5,3 +8,21 @@ class TaskStatus(object):
 
     def send_status(self, status):
         print('{}:{}'.format(self.__class__, status))
+
+        code_to_str = {
+            self.DONE: 'done',
+            self.START: 'start'
+        }
+
+        r = requests.post(config['progress_report_url'], json={'id': config['task_id'], 'progress': code_to_str[status]})
+        assert r.status_code == 200
+
+
+if __name__ == "__main__":
+    config = {
+        "progress_report_url": "http://10.43.13.8:25005/redis",
+        "task_id": "121554"
+    }
+
+    ts = TaskStatus(config)
+    ts.send_status(ts.START)
