543040f789e57d51a271f136f9b484c5a1b56805
Xiaoquan Kong <u1mail2me@gmail.com>
2019-06-17 16:14:04 +0800

    bugfix

--- ioflow/task_manager/__init__.py
+++ ioflow/task_manager/__init__.py
@@ -4,13 +4,13 @@ from ioflow.task_manager.db_task_manager import DbTaskManager
 task_manager_class_registry = {}
 
 
-def get_performance_metrics_class(config):
+def get_task_manager_class(config):
     return task_manager_class_registry[config.get('task_manager_schema', 'local')]
 
 
-def registry_performance_metrics_class(schema, class_):
+def registry_task_manager_class(schema, class_):
     task_manager_class_registry[schema] = class_
 
 
-registry_performance_metrics_class('local', LocalTaskManager)
-registry_performance_metrics_class('db', DbTaskManager)
+registry_task_manager_class('local', LocalTaskManager)
+registry_task_manager_class('db', DbTaskManager)
