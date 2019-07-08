21630fbdc3f3b0e3e38e6f7022f0bd96d585207a
Xiaoquan Kong <u1mail2me@gmail.com>
2019-07-08 11:23:39 +0800

    Refactor model_saver function

--- ioflow/model_saver/__init__.py
+++ ioflow/model_saver/__init__.py
@@ -1,26 +1,21 @@
 model_saver_mapping = {}
 
 
-def registry_model_saver(data_source_scheme, corpus_processor_class):
+def registry_model_saver_class(data_source_scheme, corpus_processor_class):
     model_saver_mapping[data_source_scheme] = corpus_processor_class
 
 
-def get_model_saver(data_source_scheme):
-    return model_saver_mapping[data_source_scheme]
+def get_model_saver_class(config):
+    return model_saver_mapping[config.get('model_saver_scheme', 'raw')]
 
 
-class ModelSaver(object):
-    def __init__(self, config):
-        self.config = config
-        model_saver_class = get_model_saver(config.get('model_saver_scheme', 'raw'))
-        self.model_saver = model_saver_class(config)
-
-    def save_model(self, *args, **kwargs):
-        self.model_saver.save_model(*args, **kwargs)
+def get_model_saver(config):
+    model_saver_class = get_model_saver_class(config)
+    return model_saver_class(config)
 
 
 from ioflow.model_saver.raw_model_saver import RawModelSaver
-registry_model_saver('raw', RawModelSaver)
+registry_model_saver_class('raw', RawModelSaver)
 
 from ioflow.model_saver.http_based_model_saver import HttpBasedModelSaver
-registry_model_saver('http', HttpBasedModelSaver)
+registry_model_saver_class('http', HttpBasedModelSaver)
