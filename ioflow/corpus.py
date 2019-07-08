5869e9743cc7b357971e3594bda307dc565ce226
Xiaoquan Kong <u1mail2me@gmail.com>
2019-07-08 11:13:37 +0800

    Refactor corpus functions

--- ioflow/corpus.py
+++ ioflow/corpus.py
@@ -1,39 +1,24 @@
 corpus_processor_mapping = {}
 
 
-def registry_corpus_processor(data_source_scheme, corpus_processor_class):
+def registry_corpus_processor_class(data_source_scheme, corpus_processor_class):
     corpus_processor_mapping[data_source_scheme] = corpus_processor_class
 
 
-def get_corpus_processor(data_source_scheme):
+def get_corpus_processor_class(data_source_scheme):
     return corpus_processor_mapping[data_source_scheme]
 
 
-class Corpus(object):
-    EVAL = 'eval'
-    TRAIN = 'train'
-
-    def __init__(self, config):
-        self.config = config
-        self.dataset_mapping = {}
-        corpus_processor_class = get_corpus_processor(config['data_source_scheme'])
-        self.corpus_processor = corpus_processor_class(config)
-
-    def prepare(self):
-        return self.corpus_processor.prepare()
-
-    def get_generator_func(self, data_set):
-        return self.corpus_processor.get_generator_func(data_set)
-
-    def get_meta_info(self):
-        return self.corpus_processor.get_meta_info()
+def get_corpus_processor(config):
+    corpus_processor_class = get_corpus_processor_class(config.get('data_source_scheme', 'raw'))
+    return corpus_processor_class(config)
 
 
 from ioflow.corpus_processor.raw_corpus_processor import RawCorpusProcessor
-registry_corpus_processor('raw', RawCorpusProcessor)
+registry_corpus_processor_class('raw', RawCorpusProcessor)
 
 from ioflow.corpus_processor.local_corpus_processor import LocalCorpusProcessor
-registry_corpus_processor('local', LocalCorpusProcessor)
+registry_corpus_processor_class('local', LocalCorpusProcessor)
 
 from ioflow.corpus_processor.http_corpus_processor import HttpCorpusProcessor
-registry_corpus_processor('http', HttpCorpusProcessor)
+registry_corpus_processor_class('http', HttpCorpusProcessor)
