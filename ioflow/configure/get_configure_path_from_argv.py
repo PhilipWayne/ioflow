f8122c7a513205fc0f8a7cccde595565d3e72ee0
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-04 16:49:50 +0800

    bugfix

--- ioflow/configure/get_configure_path_from_argv.py
+++ ioflow/configure/get_configure_path_from_argv.py
@@ -5,7 +5,7 @@ def get_configure_path_from_argv():
     parser = argparse.ArgumentParser()
     parser.add_argument('--ioflow_default_configure', type=str, nargs='?', help='specific default configure file location')
 
-    args = parser.parse_args("--ioflow_default_configure xxx".split())
+    args = parser.parse_args()
 
     return args.ioflow_default_configure
 
