482a8b54d1ae4ef7ef5980c44d542116b83769a2
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-12 20:08:53 +0800

    Support multiply configure format

--- ioflow/configure/read_configure.py
+++ ioflow/configure/read_configure.py
@@ -6,31 +6,59 @@ import os
 from ioflow.configure.get_configure_path_from_argv import get_configure_path_from_argv
 
 
-def read_configure(
-        return_empty=False,
-        default_configure='./configure.json',
-        builtin_configure='./builtin_configure.json') -> dict:
+def guess_configure_file_type(file_name):
+    file_extension_mapping = {".json": "json", ".yaml": "yaml", ".yml": "yaml"}
+
+    _, file_extension = os.path.splitext(file_name)
+
+    if file_extension in file_extension_mapping:
+        return file_extension_mapping[file_extension]
+
+    raise ValueError(file_extension)
+
+
+def find_best_file_candidate(candidate_list):
+    for candidate in candidate_list:
+        if os.path.exists(candidate):
+            return candidate
+
+        return None
+
+
+def read_configure(return_empty=False) -> dict:
     # set return_empty to True for not read config from env
     # which can prevent unexpected result
     # e.g. './configure.json' is not for this app, but for other using
     if return_empty:
         return {}
 
+    default_configure_candidate = [".".join(["./configure", ext]) for ext in ["yaml", "yml", "json"]]
+    builtin_configure_candidate = [".".join(["./builtin_configure", ext]) for ext in ["yaml", "yml", "json"]]
+
+    default_configure = find_best_file_candidate(default_configure_candidate)
+    builtin_configure = find_best_file_candidate(builtin_configure_candidate)
+
     active_configure_file = get_configure_path_from_argv()
     if not active_configure_file:
-        active_configure_file = os.getenv('_DEFAULT_CONFIG_FILE', default_configure)
+        active_configure_file = os.getenv("_DEFAULT_CONFIG_FILE", default_configure)
 
-    builtin_configure_file = os.getenv('_BUILTIN_CONFIG_FILE', builtin_configure)
+    builtin_configure_file = os.getenv("_BUILTIN_CONFIG_FILE", builtin_configure)
 
     # disable read configure from environment
     # Pconf.env()
 
-    Pconf.file(active_configure_file, encoding='json')
+    if not os.path.exists(active_configure_file):
+        print(">>> default configure file is not found!")
+        raise ValueError("default configure file is not found!")
+
+    file_encoding = guess_configure_file_type(active_configure_file)
+    Pconf.file(active_configure_file, file_encoding)
 
     # try loading builtin configure file
-    if os.path.exists(builtin_configure_file):
+    if builtin_configure_file and os.path.exists(builtin_configure_file):
         print("loading builtin configure from {}".format(builtin_configure_file))
-        Pconf.file(builtin_configure_file, encoding='json')
+        file_encoding = guess_configure_file_type(builtin_configure_file)
+        Pconf.file(builtin_configure_file, encoding=file_encoding)
     else:
         print(">>> builtin configure file is not found!")
 
