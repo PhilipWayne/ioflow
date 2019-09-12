3683a75e3cc81028999f7047a9cbf7e31ecbe17c
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-12 16:31:47 +0800

    Update setup: deal with tensorflow installation smartly

--- setup.py
+++ setup.py
@@ -1,20 +1,47 @@
 import setuptools
 from setuptools import setup
 
+
+def is_tensorflow_installed():
+    """
+    detect if tensorflow (no matter CPU or GPU based) installed
+
+    :return: bool, True for tensorflow installed
+    """
+    import importlib
+
+    try:
+        importlib.import_module("tensorflow")
+    except ModuleNotFoundError:
+        return False
+
+    return True
+
+
+# without tensorflow by default
+install_requires = [
+    "numpy",
+    "requests",
+    "flask",
+    "scikit-learn",
+    "jsonlines",
+    "pconf",
+    "tokenizer_tools",
+]
+
+if not is_tensorflow_installed():
+    install_requires.append("tensorflow")  # Will install CPU based TensorFlow
+
+
 setup(
-    name='ioflow',
-    version='0.6.2',
+    name="ioflow",
+    version="0.6.2",
     packages=setuptools.find_packages(),
-    setup_requires=[
-        'tensorflow',
-        'tokenizer_tools'
-    ],
-    url='https://github.com/howl-anderson/ioflow',
-    license='Apache 2.0',
-    author='Xiaoquan Kong',
-    author_email='u1mail2me@gmail.com',
-    description='Input/Output abstraction layer for machine learning',
-    install_requires=['numpy', 'requests', 'flask', 'scikit-learn',
-                      'jsonlines', 'pconf', 'tokenizer_tools'],
-    tests_require=['requests-mock', 'pytest']
+    url="https://github.com/howl-anderson/ioflow",
+    license="Apache 2.0",
+    author="Xiaoquan Kong",
+    author_email="u1mail2me@gmail.com",
+    description="Input/Output abstraction layer for machine learning",
+    install_requires=install_requires,
+    tests_require=["requests-mock", "pytest"],
 )
