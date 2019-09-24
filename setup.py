1fc5ebd198f41817bc7e1898afaab494fbef5af3
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-24 19:43:28 +0800

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if not is_tensorflow_installed():
 
 setup(
     name="ioflow",
-    version="0.7.2",
+    version="0.7.3",
     packages=setuptools.find_packages(),
     url="https://github.com/howl-anderson/ioflow",
     license="Apache 2.0",
